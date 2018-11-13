86b718ce16b0636351e9e8d3dbf85262ab62eead
gerelma <gerelma.faust@gmail.com>
2018-11-13 22:09:48 +0100

    error eliminate

--- src/components/PlaceHolder.js
+++ src/components/PlaceHolder.js
@@ -1,19 +1,55 @@
 import React, { Component } from "react";
 import { getItems } from "../services/placeHolderService";
-import _ from "lodash";
 
 class PlaceHolder extends Component {
   state = {
-    items: []
+    items: [],
+    isLoading: false,
+    error: null
   };
 
   async componentDidMount() {
-    const { data: items } = await getItems();
-    console.log(items);
-    this.setState({ items });
+    try {
+      const { data: items } = await getItems();
+      console.log(items);
+      this.setState({ items });
+    } catch (error) {
+      this.setState({
+        error,
+        isLoading: false
+      });
+    }
+  }
+  //test
+
+  getRows() {
+    console.log(this.state.items);
+    if (this.state.items) {
+      return this.state.items.map((anObjectMapped, index) => {
+        return (
+          <p key={anObjectMapped.id}>
+            <span>"Title: " {anObjectMapped.title} </span>
+            <br />
+            <br />
+            <span>"Body: " {anObjectMapped.body} </span>
+          </p>
+        );
+      });
+    } else {
+      return <p>data is not available</p>;
+    }
   }
   render() {
-    return <div className="row">hi</div>;
+    const { isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+    return <div className="row">{this.getRows()}</div>;
   }
 }
 
